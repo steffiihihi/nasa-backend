f64be979b18c8f8e2f358cae83fb2f1c3207bad0
steffiihihi <118903812+steffiihihi@users.noreply.github.com>
2024-07-06 23:44:39 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -15,7 +15,7 @@ app.use(cors({
 app.use(express.json())
 // app.use(express.static(path.join(__dirname,'..','public')))
 
-app.use('/v1',api)
+app.use('/',api)
 
 // app.get('/*',(req,res)=>{
     // res.sendFile(path.join(__dirname,'..','public','index.html'))

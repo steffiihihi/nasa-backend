0d0ace4b35304be379f460e9e323adb3f3fc48ac
steffiihihi <118903812+steffiihihi@users.noreply.github.com>
2024-07-06 23:39:06 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -6,18 +6,19 @@ const app=express()
 const api=require('./routes/api')
 
 app.use(cors({
-    origin: 'http://localhost:3000'
+    origin: 'http://localhost:3000',
+    origin: 'https://nasa-frontendb.vercel.app/'
 }))
 
 // app.use(morgan('combined'))
 
 app.use(express.json())
-app.use(express.static(path.join(__dirname,'..','public')))
+// app.use(express.static(path.join(__dirname,'..','public')))
 
 app.use('/v1',api)
 
-app.get('/*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'..','public','index.html'))
-})
+// app.get('/*',(req,res)=>{
+    // res.sendFile(path.join(__dirname,'..','public','index.html'))
+// })
 
 module.exports=app
